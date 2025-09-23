6b69a06eb24b43d47a43674751aaf4c3be830046
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-09-23 15:33:42 +0300

    [TTS-184] Implement Email Sender (#2)
    https://psavchenkoad.atlassian.net/browse/TTS-184?atlOrigin=eyJpIjoiOWE4ZWExMDJkNjA4NDFjODhmZmRjMzJjOWQzNTZkMDgiLCJwIjoiaiJ9

---
+++ src/Emails/SendEmailRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TagTheSpot.Services.Email.Emails
+{
+    public sealed record SendEmailRequest<TModel>(
+        string RecipientEmail,
+        string Subject,
+        TModel Model);
+}
