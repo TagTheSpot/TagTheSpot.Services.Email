ba5d19724be996a6e44c549c3be1c28973f63f9d
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-09-24 16:59:00 +0300

    [TTS-187] Implement Send Confirmation Email Requested Event Consumer (#3)
    https://psavchenkoad.atlassian.net/browse/TTS-187?atlOrigin=eyJpIjoiZTY1Mzk5MThiZDU0NGYzOTliYzQ0NmI2Mzk5ZjExZjIiLCJwIjoiaiJ9

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,12 @@
+using MassTransit;
+using Microsoft.Extensions.Options;
 using System.Net;
 using System.Net.Mail;
+using TagTheSpot.Services.Email.Consumers;
 using TagTheSpot.Services.Email.Emails;
 using TagTheSpot.Services.Email.Options;
-using TagTheSpot.Services.Shared.Messaging.Options;
+using TagTheSpot.Services.Shared.Infrastructure.Options;
+using TagTheSpot.Services.Shared.Messaging.Auth;
 
 namespace TagTheSpot.Services.Email
 {
@@ -16,6 +20,39 @@ namespace TagTheSpot.Services.Email
                 .BindConfiguration(SmtpSettings.SectionName)
                 .ValidateDataAnnotations()
                 .ValidateOnStart();
+
+            builder.Services.AddOptions<RabbitMqSettings>()
+                .BindConfiguration(RabbitMqSettings.SectionName)
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            builder.Services.AddOptions<MessagingSettings>()
+                .BindConfiguration(MessagingSettings.SectionName)
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            builder.Services.AddMassTransit(cfg =>
+            {
+                cfg.AddConsumer<SendConfirmationEmailRequestedEventConsumer>();
+
+                cfg.UsingRabbitMq((context, config) =>
+                {
+                    var rabbitMqSettings = context.GetRequiredService<IOptions<RabbitMqSettings>>().Value;
+                    var messagingSettings = context.GetRequiredService<IOptions<MessagingSettings>>().Value;
+
+                    config.Host(rabbitMqSettings.Host, rabbitMqSettings.VirtualHost, h =>
+                    {
+                        h.Username(rabbitMqSettings.Username);
+                        h.Password(rabbitMqSettings.Password);
+                    });
+
+                    config.ReceiveEndpoint(messagingSettings.QueueName, e =>
+                    {
+                        e.Bind<SendConfirmationEmailRequestedEvent>();
+                        e.ConfigureConsumer<SendConfirmationEmailRequestedEventConsumer>(context);
+                    });
+                });
+            });
 
             var smtpSettings = builder.Configuration.GetRequiredSection(SmtpSettings.SectionName).Get<SmtpSettings>();
 
