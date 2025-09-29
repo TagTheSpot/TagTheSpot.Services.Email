1061a94d687b5336fb585e8bf5b13dfce4ac1e14
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-09-29 16:32:42 +0300

    [TTS-195] Implement Send Submission Rejected Email Requested Event Consumer (#6)
    https://psavchenkoad.atlassian.net/browse/TTS-195?atlOrigin=eyJpIjoiMzc5NzdmZDA3YjlkNGQ3ZjhmNjcwYTM4MmU4ZDk3MjgiLCJwIjoiaiJ9

---
+++ src/Consumers/SendSubmissionRejectedEmailRequestedEventConsumer.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using TagTheSpot.Services.Email.Emails;
+using TagTheSpot.Services.Shared.Messaging.Submissions;
+
+namespace TagTheSpot.Services.Email.Consumers
+{
+    public sealed class SendSubmissionRejectedEmailRequestedEventConsumer
+        : IConsumer<SendSubmissionRejectedEmailRequestedEvent>
+    {
+        private readonly IEmailSender _emailSender;
+
+        public SendSubmissionRejectedEmailRequestedEventConsumer(
+            IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public async Task Consume(ConsumeContext<SendSubmissionRejectedEmailRequestedEvent> context)
+        {
+            var message = context.Message;
+
+            var request = new SendEmailRequest<SendSubmissionRejectedEmailRequestedEvent>(
+                RecipientEmail: message.Recipient,
+                Subject: "❌ Вашу заявку відхилено на TagTheSpot",
+                message);
+
+            await _emailSender.SendEmailAsync(
+                request,
+                templatePath: "Templates/SubmissionRejectedTemplate.cshtml");
+        }
+    }
+}
